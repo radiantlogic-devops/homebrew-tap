ae12112632bce8e8e58734f47c38a99d659414c2
releaserbot <releaserbot@users.noreply.github.com>
2025-10-31 16:50:19 +0000

    Update formula to version v1.0.0-beta.1

--- Formula/eocctl.rb
+++ Formula/eocctl.rb
@@ -1,12 +1,12 @@
 class Eocctl < Formula
   desc "Official EOC CLI tool."
   homepage "https://github.com/radiantlogic-devops/eocctl"
-  version "1.0.0-beta.1"
+  version "v1.0.0-beta.1"
   depends_on :macos
 
   if Hardware::CPU.arm?
     url "https://github.com/radiantlogic-devops/rlitools-dist/releases/download/v1.0.0/rlitools_1.0.0_darwin_arm64.tar.gz"
-    sha256 "ed82bd2813364cdd1aa536f3961c0ce3ba0023c4acf1e79abbde6abd1d44ebbb"
+    sha256 "6c876431316a658bb0bbfffa1b25243abaff1ae0713918c1644d85288678101a"
 
       def install
         system "./rlitools", "download", "https://dist.saas.radiantlogic.com/releases/eocctl/v1.0.0-beta.1/darwin/eocctl-darwin-arm64"
